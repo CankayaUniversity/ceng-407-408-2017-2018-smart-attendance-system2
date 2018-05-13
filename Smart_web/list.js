e11cb72631613944ae5e29e7573c4abfd89f8d4f
BerkayCinarrr <berkaycinar@icloud.com>
2018-05-13 22:49:11 +0300

    WebApp
    WebApp

---
+++ Smart_web/list.js
@@ -0,0 +1,14 @@
+﻿var db = firebase.firestore();
+
+
+
+
+db.collection("Students").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+
+            list_div.innerHTML += " <div class='list-item'><h3>" + doc.data().name + "</h3></div>"
+        });
+});
+
+
+
